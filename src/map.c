11f1dbc6e2217ba1c5752a67d91d76511a50f038
Koshiro Nakamura <kosnakam@c3r1s1.42tokyo.jp>
2024-10-11 16:33:31 +0900

    mapが扱いやすくなりました

--- src/map.c
+++ src/map.c
@@ -1,6 +1,16 @@
 #include "cub3D.h"
 
-int check_map_spell(char **argv)
+int	map_check(t_map *map_info)
+{
+	if (!map_info->map || !map_info->map[0])
+		return (1);
+	if (!map_info->no || !map_info->so || !map_info->we || !map_info->ea
+		|| map_info->f == -1 || map_info->c == -1)
+		return (1);
+	return (0);
+}
+
+int	check_map_spell(char **argv)
 {
 	if (ft_strrchr(argv[1], '.') == 0
 		|| ft_strncmp(ft_strrchr(argv[1], '.'), ".cub", 4) != 0)
@@ -15,19 +25,23 @@ int	check_path_space(char *path)
 	return (0);
 }
 
-void	set_path(char **target, char *map)
+int	set_path(char **target, char *map)
 {
 	while (*map == 32 || (*map >= 9 && *map <= 13))
 		map++;
 	*target = ft_strdup(map);
+	return (0);
 }
 
 int	set_color(int *target, char *map)
 {
+	int	i;
 	int	*rgb;
-	int	i = -1;
 
+	i = -1;
 	rgb = (int *)malloc(sizeof(int) * 3);
+	if (!rgb)
+		return (1);
 	while (*map == 32 || (*map >= 9 && *map <= 13))
 		map++;
 	while (++i < 3)
@@ -40,53 +54,50 @@ int	set_color(int *target, char *map)
 		else
 			return (1);
 	}
-	free(rgb);
 	*target = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+	free(rgb);
 	return (0);
 }
 
 int	set_map_info(t_map *map_info, char *map)
 {
 	if (ft_strncmp(map, "NO", 2) == 0 && check_path_space(map += 2))
-		set_path(&map_info->no, map);
+		return (set_path(&map_info->no, map));
 	else if (ft_strncmp(map, "SO", 2) == 0 && check_path_space(map += 2))
-		set_path(&map_info->so, map);
+		return (set_path(&map_info->so, map));
 	else if (ft_strncmp(map, "WE", 2) == 0 && check_path_space(map += 2))
-		set_path(&map_info->we, map);
+		return (set_path(&map_info->we, map));
 	else if (ft_strncmp(map, "EA", 2) == 0 && check_path_space(map += 2))
-		set_path(&map_info->ea, map);
+		return (set_path(&map_info->ea, map));
 	else if (ft_strncmp(map, "F", 1) == 0 && check_path_space(map += 1))
 		return (set_color(&map_info->f, map));
 	else if (ft_strncmp(map, "C", 1) == 0 && check_path_space(map += 1))
 		return (set_color(&map_info->c, map));
-	return (0);
+	return (2);
 }
 
-int	map_check(t_map *map_info)
+int	map_info_init(t_map **map_info, char *argv)
 {
-	int	column = -1;
+	int	count = 0;
+	int	fd;
+	char	buffer[1];
+	int bytes = 1;
 
-	if (!map_info->map || !map_info->map[0])
-		return (1);
-	while (map_info->map[++column])
+	buffer[0] = '\0';
+	fd = open(argv, O_RDONLY);
+	while (bytes == 1)
 	{
-		if (set_map_info(map_info, map_info->map[column]))
-			return (1);
-		// if (set_map(map_info, map_info->map[column]))
-		// 	return (1);
+		bytes = read(fd, buffer, 1);
+		if (bytes < 1)
+			break ;
+		if (buffer[0] == '\n')
+			count++;
 	}
-	ft_printf("map_info->map : %s\n", map_info->map[0]);
-	if (!map_info->no || !map_info->so || !map_info->we || !map_info->ea
-		|| map_info->f == -1 || map_info->c == -1)
-		return (1);
-	return (0);
-}
-
-int	map_info_init(t_map **map_info)
-{
-	(*map_info)->map = (char **)malloc(sizeof(char *) * 30);
+	close(fd);
+	(*map_info)->map = (char **)malloc(sizeof(char *) * (count - 5));
 	if (!(*map_info)->map)
 		return (1);
+	(*map_info)->map[count - 6] = NULL;
 	(*map_info)->no = NULL;
 	(*map_info)->so = NULL;
 	(*map_info)->we = NULL;
@@ -100,17 +111,26 @@ int	map_scan(t_map *map_info, char *argv)
 {
 	int		y;
 	int		fd;
+	char	*line;
 
 	y = 0;
 	fd = open(argv, O_RDONLY);
-	if (fd == -1 || map_info_init(&map_info))
+	if (fd == -1 || map_info_init(&map_info, argv))
 		return (1);
 	while (1)
 	{
-		map_info->map[y] = get_next_line(fd);
-		if (!map_info->map[y])
+		line = get_next_line(fd);
+		if (!line)
 			break ;
+		if (line[0] == '\n' || set_map_info(map_info, line) == 0)
+		{
+			free(line);
+			continue ;
+		}
+		map_info->map[y] = ft_strdup(line);
+		free(line);
 		y++;
 	}
+	close(fd);
 	return (map_check(map_info));
 }
